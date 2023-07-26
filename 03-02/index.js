519e7ba7b3427e9d20f2b16b141be6a9fcc2288a
fahrulraziws2610 <fahrulrazi.wijayasaputra2610@gmail.com>
2023-07-26 12:11:10 +0700

    belajar js

---
+++ 03-02/index.js
@@ -0,0 +1,30 @@
+//penggunaan let
+let panjang = 3;
+console.log(panjang);
+
+//penggunaan let
+const y = 4
+console.log(y)
+
+// penggunaan bolean
+let anime = false;
+if(anime == 3){
+    true
+} else {
+    false
+}
+console.log(anime);
+
+// penggunaan string
+let a = "Nama Saya adalah" 
+let b = " Fahrul"
+
+console.log(a + b)
+
+// penggunaan number
+let d = 3
+let e = 4
+let hasil = d + e
+console.log(d)
+console.log(e)
+console.log("hasil dari 3 + 4 = " + hasil)
