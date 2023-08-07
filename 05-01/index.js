da17bc50d094c277d5b6795b8c248559ed99d6fd
fahrulraziws2610 <fahrulrazi.wijayasaputra2610@gmail.com>
2023-08-07 15:47:11 +0700

    Pemrograman Fungsional

---
+++ 05-01/index.js
@@ -0,0 +1,24 @@
+function luaspersegi() {
+    var nilaix = document.getElementById("nilaix")
+    let luas = 0;
+
+    function total(){
+        const nilai = nilaix.value;
+        if(nilai > 0){
+            luas = nilai * nilai
+            return luas
+        }
+    }
+    return total
+}
+
+const persegi = luaspersegi()
+const luas = document.getElementById("hasilkali");
+const btn = document.getElementById("hitungperkalian");
+
+console.log(luas)
+
+btn.addEventListener("click", function() {
+    const count = persegi();
+    luas.textContent = count;
+})
