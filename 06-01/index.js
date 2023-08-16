ad963e3136ed5450d90bd997b9deb98018fd28e1
fahrulraziws2610 <fahrulrazi.wijayasaputra2610@gmail.com>
2023-08-16 17:00:48 +0700

    penanganan error

--- 06-01/index.js
+++ 06-01/index.js
@@ -1,13 +1,41 @@
-class formLogin{
-    constructor(selector = '', onSubmit = () => {}) {
-        const form = document.querySelector(selector);
+// class formLogin{
+//     constructor(selector = '', onSubmit = () => {}) {
+//         const form = document.querySelector(selector);
 
-        form.addEventListener('submit', e => {
+//         form.addEventListener('submit', e => {
+//             e.preventDefault();
+//             const username = form.querySelector('input#username').value;
+//             const password = form.querySelector('input#password').value;
+//             onSubmit(username, password)
+//         })
+//     } 
+// }
+
+// new formLogin('form#form-login', (username, password) => {
+//     console.log('tes', {username, password});
+// });
+
+class form{
+    constructor(selectform, selectinput = [], onSubmit = () => {}) {
+        const element = document.querySelector(selectform);
+
+        element.addEventListener('submit', e => {
             e.preventDefault();
-            const username = form.querySelector('input#username').value;
-            const password = form.querySelector('input#password').value;
-            onSubmit(username, password)
-        })
+            const value = [];
+            selectinput.forEach((sel) => {
+                const input = document.querySelector(sel).value;
+                value.push(input);
+            })
+            onSubmit(value);
+        });
+    }
+}
+
+class formLogin extends form {
+    constructor(selectform, onSubmit = () => {}) {
+        super(selectform, ['input#username', 'input#password'], ([username, password]) => {
+            onSubmit(username, password);
+        });
     }
 }
 
