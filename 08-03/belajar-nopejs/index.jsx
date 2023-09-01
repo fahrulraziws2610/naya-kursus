65af38e45614166cca675588fb445cfdecd1cebb
fahrulraziws2610 <fahrulrazi.wijayasaputra2610@gmail.com>
2023-09-01 15:06:53 +0700

    node dan react

---
+++ 08-03/belajar-nopejs/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import Home from "./pages/home";
+
+render(<Home></Home>, document.querySelector('#app'))
---
+++ 08-03/belajar-nopejs/pages/home.jsx
@@ -0,0 +1,3 @@
+export default () => {
+    return (<h1>welcome to react</h1>)
+}
